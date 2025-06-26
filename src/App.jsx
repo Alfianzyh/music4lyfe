f0a34b005f716367bfc2c3879402981918bc54ab
YaXioz <alfianzyh@gmail.com>
2025-06-26 21:56:58 +0700

    adding structure tailwind n react file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="bg-black text-white font-sans">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
