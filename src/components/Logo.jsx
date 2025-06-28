3bb538c19f4c28410a590ac7026911aa33d0c138
YaXioz <alfianzyh@gmail.com>
2025-06-28 14:15:55 +0700

    update style logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
 // src/components/Logo.jsx
-const Logo = ({ size = 8, textSize = "text-2xl" }) => {
+const Logo = ({ size = 5, textSize = "text-2xl" }) => {
   return (
     <div className="flex items-center space-x-3">
       <img
         src="/public/logo.png"
         alt="Music4Lyfe Logo"
-        className="w-[65px] h-[59px]"
+        className="w-[60px] h-[50px] -ml-3"
       />
       <span className={`${textSize} text-white font-bold font-poppins`}>
         Music4Lyfe
