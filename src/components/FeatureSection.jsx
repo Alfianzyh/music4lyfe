f09d92151af0ab329aa1755dce39c53eaff92985
YaXioz <alfianzyh@gmail.com>
2025-06-29 19:02:00 +0700

    update icon feature

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FeatureCard from "./FeatureCard";
-import { FaHeadphonesAlt, FaCompactDisc, FaRobot, FaCloudDownloadAlt, FaShareAlt, FaMobileAlt } from "react-icons/fa";
+import { FaRobot, FaDownload , FaShareAlt, FaMobileAlt } from "react-icons/fa";
+import { PiInfinityBold } from "react-icons/pi";
+import { FaHeadphones } from "react-icons/fa6";
 
 const FeatureSection = () => {
   return (
@@ -13,27 +15,27 @@ const FeatureSection = () => {
       </div>
 
       <div className="mt-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto">
-        <FeatureCard icon={<FaHeadphonesAlt className="text-purple-300 w-6 h-6" />} title="Unlimited Streaming">
+        <FeatureCard icon={<PiInfinityBold  className="text-purple-300 w-10 h-10" />} title="Unlimited Streaming">
           Access millions of songs without limits. Stream your favorite tracks anytime, anywhere.
         </FeatureCard>
 
-        <FeatureCard icon={<FaCompactDisc className="text-purple-300 w-6 h-6" />} title="Hi-Fi Audio Quality">
+        <FeatureCard icon={<FaHeadphones className="text-purple-300 w-7 h-7" />} title="Hi-Fi Audio Quality">
           Experience crystal-clear sound with lossless audio streaming up to 24-bit/192kHz.
         </FeatureCard>
 
-        <FeatureCard icon={<FaRobot className="text-purple-300 w-6 h-6" />} title="Smart Recommendations">
+        <FeatureCard icon={<FaRobot className="text-purple-300 w-8 h-8" />} title="Smart Recommendations">
           AI-powered suggestions that learn your taste and help you discover music you'll love.
         </FeatureCard>
 
-        <FeatureCard icon={<FaCloudDownloadAlt className="text-purple-300 w-6 h-6" />} title="Offline Listening">
+        <FeatureCard icon={<FaDownload  className="text-purple-300 w-6 h-6" />} title="Offline Listening">
           Download your favorite songs and playlists to enjoy music without internet connection.
         </FeatureCard>
 
-        <FeatureCard icon={<FaShareAlt className="text-purple-300 w-6 h-6" />} title="Social Sharing">
+        <FeatureCard icon={<FaShareAlt className="text-purple-300 w-7 h-7" />} title="Social Sharing">
           Share your favorite tracks and playlists with friends and see what they’re listening to.
         </FeatureCard>
 
-        <FeatureCard icon={<FaMobileAlt className="text-purple-300 w-6 h-6" />} title="Cross-Platform">
+        <FeatureCard icon={<FaMobileAlt className="text-purple-300 w-7 h-7" />} title="Cross-Platform">
           Seamlessly sync your music across all devices - phone, tablet, computer, and smart speakers.
         </FeatureCard>
       </div>
