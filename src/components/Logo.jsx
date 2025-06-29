f504df10719ef34a09a2646fbb003e396737fd4b
YaXioz <alfianzyh@gmail.com>
2025-06-29 19:18:10 +0700

    update nav logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ const Logo = ({ size = 5, textSize = "text-2xl" }) => {
   return (
     <div className="flex items-center space-x-3">
       <img
-        src="/public/logo.png"
+        src="src/assets/Logo.png"
         alt="Music4Lyfe Logo"
         className="w-[60px] h-[50px] -ml-3"
       />
