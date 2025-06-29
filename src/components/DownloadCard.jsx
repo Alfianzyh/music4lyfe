cbd36991081d1bd34fad7f7337e30ee273328650
YaXioz <alfianzyh@gmail.com>
2025-06-29 11:42:20 +0700

    update cursor

--- src/components/DownloadCard.jsx
+++ src/components/DownloadCard.jsx
@@ -1,7 +1,7 @@
 // src/components/DownloadCard.jsx
 const DownloadCard = ({ icon, title, subtitle }) => {
   return (
-    <div className="flex items-center w-fit px-5 py-3 rounded-xl bg-black text-white hover:bg-white/10 transition space-x-4">
+    <div className="flex items-center w-fit px-5 py-3 rounded-xl bg-black text-white hover:bg-white/10 transition space-x-4 cursor-pointer">
       <div className="text-white text-2xl">{icon}</div>
       <div>
         <p className="text-xs text-white">{subtitle}</p>
