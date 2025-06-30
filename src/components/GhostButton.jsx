c1dc0ede2e43d9ed7c43552984b8f4850ff25fca
YaXioz <alfianzyh@gmail.com>
2025-06-30 10:34:41 +0700

    update responsive ghost button

--- src/components/GhostButton.jsx
+++ src/components/GhostButton.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { FaDownload } from "react-icons/fa";
 
-const GhostButton = ({ children }) => (
-  <button className="px-8 py-3 border border-white/20 outline outline-1 outline-offset-[-1px] outline-white/20 bg-white/10 text-white rounded-full flex items-center gap-2 hover:bg-white/20 transition">
-    <FaDownload className="text-white" />
+const GhostButton = ({ children, icon = <FaDownload />, onClick }) => (
+  <button
+    onClick={onClick}
+    className="px-8 py-3 border border-white/20 outline outline-1 outline-offset-[-1px] outline-white/20 bg-white/10 text-white rounded-full flex items-center gap-2 hover:bg-white/20 transition"
+  >
+    {icon}
     {children}
   </button>
 );
