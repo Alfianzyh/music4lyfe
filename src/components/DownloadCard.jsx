334525a4f70155d534dd993b96857d37fcf692a9
YaXioz <alfianzyh@gmail.com>
2025-06-30 10:27:39 +0700

    update responsive download card

--- src/components/DownloadCard.jsx
+++ src/components/DownloadCard.jsx
@@ -1,10 +1,10 @@
 const DownloadCard = ({ icon, title, subtitle }) => {
   return (
-    <div className="flex items-center w-fit px-5 py-3 rounded-xl bg-black text-white hover:bg-white/10 transition space-x-4 cursor-pointer">
-      <div className="text-white text-2xl">{icon}</div>
-      <div>
-        <p className="text-xs text-white">{subtitle}</p>
-        <p className="text-lg font-semibold text-white">{title}</p>
+    <div className="flex items-center w-full sm:w-fit px-4 sm:px-5 py-3 rounded-xl bg-black text-white hover:bg-white/10 transition space-x-3 sm:space-x-4 cursor-pointer">
+      <div className="text-xl sm:text-2xl">{icon}</div>
+      <div className="leading-tight">
+        <p className="text-xs sm:text-sm text-white/70">{subtitle}</p>
+        <p className="text-sm sm:text-lg font-semibold text-white">{title}</p>
       </div>
     </div>
   );
